79df7967cad3de7fefbb6b48849432b61210eb9d
J C Lisboa <lookjota@gmail.com>
2025-02-20 17:21:04 -0300

    FULLy

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,8 +1,32 @@
+import 'dotenv/config'
+
 import fastify from 'fastify'
-import { PrismaClient } from '@prisma/client'
+import { memoriesRoutes } from './routes/memories'
+import { authRoutes } from './routes/auth'
+import jwt from '@fastify/jwt'
+// import cors from 'fastify-cors'
+
 
 const app = fastify()
-const prisma = new PrismaClient()
+
+// app.register(cors, {
+//   origin: true,
+// })
+
+app.register(jwt, {
+  secret: 'spacetime',
+})
+
+app.register(authRoutes)
+app.register(memoriesRoutes)
+
+app.listen({
+  port: 3333,
+  host: '0.0.0.0',
+}).then(() => {
+  console.log('HTTP server running on http://localhost:3333')
+})
+
 
 // HTTP Method: GET, POST, PUT, PATCH, DELETE
 /*
@@ -11,19 +35,5 @@ const prisma = new PrismaClient()
   PUT - Update, Atualizar
   PATCH - Update, Atualizar
   DELETE - Delete, Deletar
-
 */
-
-app.get('/users', async () => {
-  const users = await prisma.user.findMany()
-  
-  return users
-})
-
 // API RESTfull
-
-app.listen({
-  port: 3333,
-}).then(() => {
-  console.log('HTTP server running on http://localhost:3333')
-})
